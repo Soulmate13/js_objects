9672ee8a579e69d642c086c328b3253d551910c0
Soulmate13 <glebzhurba@gmail.com>
2019-11-18 16:22:23 +0200

    fraction calculator fully functional

--- script.js
+++ script.js
@@ -41,9 +41,6 @@ let GenerateFractions = function () {
 
     while (denominatorOne == 0);
 
-
-
-
     numeratorTwo = +prompt("Enter the numerator of the second fraction");
 
     do {
@@ -73,39 +70,112 @@ let GenerateFractions = function () {
 
 };
 
+let ReduceFraction = function (_numerator, _denominator) {
+    firstNumber = Number(_numerator);
+    secondNumber = Number(_denominator);
+    let remainder;
+
+    if ((firstNumber % secondNumber) == 0) {
+        firstNumber = firstNumber / secondNumber;
+        secondNumber = 1;
+
+        _numerator = firstNumber;
+        _denominator = secondNumber;
+    }
+
+    else {
+        while ((firstNumber % secondNumber) > 0) {
+
+            remainder = (firstNumber % secondNumber);
+
+            firstNumber = secondNumber;
+            secondNumber = remainder;
+        };
+
+        _numerator = Number(_numerator) / remainder;
+        _denominator = Number(_denominator) / remainder;
+
+    }
+
+    fractionThree.numerator = _numerator;
+    fractionThree.denominator = _denominator;
+
+};
+
+let ReduceFractions = function (_fractionOne, _fractionTwo) {
+
+    let firstNumber = Number(_fractionOne.numerator);
+    let secondNumber = Number(_fractionOne.denominator);
+    let remainder;
+
+    if ((firstNumber % secondNumber) == 0) {
+        firstNumber = firstNumber / secondNumber;
+        secondNumber = 1;
+
+        _fractionOne.numerator = firstNumber;
+        _fractionOne.denominator = secondNumber;
+    }
+
+    else {
+        while ((firstNumber % secondNumber) > 0) {
+
+            remainder = (firstNumber % secondNumber);
+
+            firstNumber = secondNumber;
+            secondNumber = remainder;
+        };
+
+        _fractionOne.numerator = Number(_fractionOne.numerator) / remainder;
+        _fractionOne.denominator = Number(_fractionOne.denominator) / remainder;
+
+    }
+
+    firstNumber = Number(_fractionTwo.numerator);
+    secondNumber = Number(_fractionTwo.denominator);
+    remainder = 0;
+
+    if ((firstNumber % secondNumber) == 0) {
+        firstNumber = firstNumber / secondNumber;
+        secondNumber = 1;
+
+        _fractionTwo.numerator = firstNumber;
+        _fractionTwo.denominator = secondNumber;
+    }
+
+    else {
+        while ((firstNumber % secondNumber) > 0) {
+
+            remainder = (firstNumber % secondNumber);
+
+            firstNumber = secondNumber;
+            secondNumber = remainder;
+        };
+
+        _fractionTwo.numerator = Number(_fractionTwo.numerator) / remainder;
+        _fractionTwo.denominator = Number(_fractionTwo.denominator) / remainder;
+
+    }
+
+    alert(`Your reduced fractions are ${fractionOne.numerator}/${fractionOne.denominator} and ${fractionTwo.numerator}/${fractionTwo.denominator}`);
+
+};
+
 let AddFractions = function (_fractionOne, _fractionTwo) {
 
     if ((_fractionOne.denominator) == (_fractionTwo.denominator)) {
-        fractionThree.denominator = _fractionOne.denominator
+        fractionThree.denominator = Number(_fractionOne.denominator)
         fractionThree.numerator = Number(_fractionOne.numerator) + Number(_fractionTwo.numerator)
-
-        if (fractionThree.denominator == fractionThree.numerator) {
-            fractionThree.denominator = 1;
-            fractionThree.numerator = 1;
-        }
-
-        else if ((fractionThree.numerator % fractionThree.denominator) == 0) {
-            fractionThree.numerator = fractionThree.numerator / fractionThree.denominator;
-            fractionThree.denominator = 1;
-        }
-
-        alert(`${fractionOne.numerator}/${fractionOne.denominator} + ${fractionTwo.numerator}/${fractionTwo.denominator} = ${fractionThree.numerator}/${fractionThree.denominator}`);
 
     }
 
     else {
         fractionThree.denominator = fractionOne.denominator * fractionTwo.denominator;
         fractionThree.numerator = Number(fractionOne.numerator * fractionTwo.denominator) + Number(fractionTwo.numerator * fractionOne.denominator);
+    };
 
-        if ((fractionThree.numerator % fractionThree.denominator) == 0) {
-            fractionThree.numerator = fractionThree.numerator / fractionThree.denominator;
-            fractionThree.denominator = 1;
-        }
+    ReduceFraction(fractionThree.numerator, fractionThree.denominator);
 
-
-        alert(`${fractionOne.numerator}/${fractionOne.denominator} + ${fractionTwo.numerator}/${fractionTwo.denominator} = ${fractionThree.numerator}/${fractionThree.denominator}`);
-
-    }
+    alert(`${fractionOne.numerator}/${fractionOne.denominator} + ${fractionTwo.numerator}/${fractionTwo.denominator} = ${fractionThree.numerator}/${fractionThree.denominator}`);
 
 };
 
@@ -114,15 +184,7 @@ let MultiplyFractions = function (_fractionOne, _fractionTwo) {
     fractionThree.denominator = fractionOne.denominator * fractionTwo.denominator;
     fractionThree.numerator = fractionOne.numerator * fractionTwo.numerator;
 
-    if (fractionThree.denominator == fractionThree.numerator) {
-        fractionThree.denominator = 1;
-        fractionThree.numerator = 1;
-    }
-
-    else if ((fractionThree.numerator % fractionThree.denominator) == 0) {
-        fractionThree.numerator = fractionThree.numerator / fractionThree.denominator;
-        fractionThree.denominator = 1;
-    }
+    ReduceFraction(fractionThree.numerator, fractionThree.denominator);
 
     alert(`${fractionOne.numerator}/${fractionOne.denominator} * ${fractionTwo.numerator}/${fractionTwo.denominator} = ${fractionThree.numerator}/${fractionThree.denominator}`);
 
@@ -132,15 +194,10 @@ let DivideFractions = function (_fractionOne, _fractionTwo) {
     fractionThree.numerator = _fractionOne.numerator * _fractionTwo.denominator;
     fractionThree.denominator = _fractionOne.denominator * _fractionTwo.numerator;
 
-    if (fractionThree.denominator == fractionThree.numerator) {
-        fractionThree.denominator = 1;
-        fractionThree.numerator = 1;
-    }
+    firstNumber = Number(fractionThree.numerator);
+    secondNumber = Number(fractionThree.denominator);
 
-    else if ((fractionThree.numerator % fractionThree.denominator) == 0) {
-        fractionThree.numerator = fractionThree.numerator / fractionThree.denominator;
-        fractionThree.denominator = 1;
-    }
+    ReduceFraction(fractionThree.numerator, fractionThree.denominator);
 
     alert(`${fractionOne.numerator}/${fractionOne.denominator} / ${fractionTwo.numerator}/${fractionTwo.denominator} = ${fractionThree.numerator}/${fractionThree.denominator}`);
 
@@ -152,18 +209,6 @@ let SubstractFractions = function (_fractionOne, _fractionTwo) {
         fractionThree.denominator = _fractionOne.denominator
         fractionThree.numerator = Number(_fractionOne.numerator) - Number(_fractionTwo.numerator)
 
-        if (fractionThree.denominator == fractionThree.numerator) {
-            fractionThree.denominator = 1;
-            fractionThree.numerator = 1;
-        }
-
-        else if ((fractionThree.numerator % fractionThree.denominator) == 0) {
-            fractionThree.numerator = fractionThree.numerator / fractionThree.denominator;
-            fractionThree.denominator = 1;
-        }
-
-        alert(`${fractionOne.numerator}/${fractionOne.denominator} - ${fractionTwo.numerator}/${fractionTwo.denominator} = ${fractionThree.numerator}/${fractionThree.denominator}`);
-
     }
 
     else {
@@ -171,12 +216,9 @@ let SubstractFractions = function (_fractionOne, _fractionTwo) {
         fractionThree.denominator = fractionOne.denominator * fractionTwo.denominator;
         fractionThree.numerator = Number(fractionOne.numerator * fractionTwo.denominator) - Number(fractionTwo.numerator * fractionOne.denominator);
 
-        if ((fractionThree.numerator % fractionThree.denominator) == 0) {
-            fractionThree.numerator = fractionThree.numerator / fractionThree.denominator;
-            fractionThree.denominator = 1;
-        }
-        alert(`${fractionOne.numerator}/${fractionOne.denominator} - ${fractionTwo.numerator}/${fractionTwo.denominator} = ${fractionThree.numerator}/${fractionThree.denominator}`);
+    };
 
-    }
+    ReduceFraction(fractionThree.numerator, fractionThree.denominator);
 
-}
+    alert(`${fractionOne.numerator}/${fractionOne.denominator} - ${fractionTwo.numerator}/${fractionTwo.denominator} = ${fractionThree.numerator}/${fractionThree.denominator}`);
+};
